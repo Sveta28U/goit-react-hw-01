d27f30cb26ace39f02fc1cb52d0e472b36181e62
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-07-14 18:36:28 +0300

    commit import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import userData from "./userData.json";
-import Profile from "../Profile/Profile";
+import Profile from "./Profile/Profile";
 
 import friends from "./friends.json";
 import FriendList from "../FriendList/FriendList";
